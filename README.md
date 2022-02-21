458cb15fb94cdc83caa8f2c066d1147a9274809d
Matthew J J <98533759+mattan27@users.noreply.github.com>
2022-02-21 17:08:47 +0000

    Chapter 3 Lab

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ Having two turtles race and drawing shapes using different methods like for loops.
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 0
 
-Grace days remaining: < # >/5
+Grace days remaining: 5/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ None
 
 ## REFERENCES:
- < List any outside resources used >
+ python.org
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+ None
--- main.py
+++ main.py
@@ -18,9 +18,59 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. your code goes here
+michelangelo.speed(1)
+leonardo.speed(1)
+x = random.randrange(1,100)
+michelangelo.fd(x)
+y = random.randrange(1,100)
+leonardo.fd(y)
+michelangelo.undo()
+leonardo.undo()
 
+for x in range (10):
+  michelangelo.fd(x)
+  leonardo.fd(x)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
 # Part B - complete part B here
+leonardo.hideturtle()
+michelangelo.pendown()
+michelangelo.pencolor('orange')
 
+sides=3
+for y in range (sides):
+    michelangelo.fd(50)
+    michelangelo.lt(360/sides)
+michelangelo.goto(-100,20)
+michelangelo.clear()
+
+sides=4
+for y in range (sides):
+    michelangelo.fd(50)
+    michelangelo.lt(360/sides)
+michelangelo.goto(-100,20)
+michelangelo.clear()
+
+sides=6
+for y in range (sides):
+    michelangelo.fd(50)
+    michelangelo.lt(360/sides)
+michelangelo.goto(-100,20)
+michelangelo.clear()
+
+sides=9
+for y in range (sides):
+    michelangelo.fd(50)
+    michelangelo.lt(360/sides)
+michelangelo.goto(-100,20)
+michelangelo.clear()
+
+sides=12
+for y in range (sides):
+    michelangelo.fd(50)
+    michelangelo.lt(360/sides)
+michelangelo.goto(-100,20)
+michelangelo.clear()
 
 window.exitonclick()
